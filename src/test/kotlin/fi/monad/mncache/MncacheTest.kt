0cfa760d2326be96704ee0e01e8d6d8787709f51
Tuomas Peltola <tuomas.peltola@monad.fi>
2022-10-14 10:33:08 +0300

    Test cached suspended function

--- src/test/kotlin/fi/monad/mncache/MncacheTest.kt
+++ src/test/kotlin/fi/monad/mncache/MncacheTest.kt
@@ -2,11 +2,23 @@ package fi.monad.mncache
 import io.micronaut.runtime.EmbeddedApplication
 import io.micronaut.test.extensions.kotest.annotation.MicronautTest
 import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
 
 @MicronautTest
-class MncacheTest(private val application: EmbeddedApplication<*>): StringSpec({
+class MncacheTest(private val service: Service): StringSpec({
 
-    "test the server is running" {
-        assert(application.isRunning)
+    "suspended service caches results by parameter" {
+        service.suspended(1) shouldBe 1
+        service.suspended(2) shouldBe 2
+    }
+
+    "suspended service without explicit parameters caches results by parameter" {
+        service.suspendedWithoutExplicitParameters(1) shouldBe 1
+        service.suspendedWithoutExplicitParameters(2) shouldBe 2
+    }
+
+    "service caches results by parameter" {
+        service.sample(1) shouldBe 1
+        service.sample(2) shouldBe 2
     }
 })
